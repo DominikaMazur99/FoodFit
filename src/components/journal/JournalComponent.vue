356fd7811edf69d1e55d79f39ec4003051054cd0
d.mazur <d.mazur@aotm.gov.pl>
2024-04-25 12:54:17 +0200

    new style to journal component

--- src/components/journal/JournalComponent.vue
+++ src/components/journal/JournalComponent.vue
@@ -39,4 +39,5 @@ export default {
 .v-window {
     max-width: 700px;
 }
+
 </style>
--- src/components/journal/MealsJournalComponent.vue
+++ src/components/journal/MealsJournalComponent.vue
@@ -1,32 +1,67 @@
 <template>
-    <v-tabs v-model="tab" color="#2f7d28">
-        <v-tab
-            v-for="(day, index) in days"
-            :key="index"
-            :value="index"
-            :class="{ active: index === currentDay }"
+    <div :style="{ marginTop: '1rem' }">
+        <v-tabs
+            v-model="tab"
+            hide-slider
         >
-            {{ day }}
-        </v-tab>
-    </v-tabs>
-    <v-window v-model="tab">
-        <v-window-item v-for="(day, index) in days" :key="index" :value="index">
-        </v-window-item>
-    </v-window>
+            <v-tab
+                v-for="(day, index) in days"
+                :key="index"
+                :value="index"
+                :class="{ active: index === selectedDay }"
+                @click="changeDay(index)"
+            >
+                {{ day }}
+            </v-tab>
+        </v-tabs>
+        <v-window v-model="tab">
+            <v-window-item
+                v-for="(day, index) in days"
+                :key="index"
+                :value="index"
+            >
+            </v-window-item>
+        </v-window>
+    </div>
 </template>
 
 <script>
 export default {
     data() {
         return {
-            days: ["Pon", "Wt", "Śr", "Cz", "Pt", "So", "Nd"], // Skróty dni tygodnia
-            currentDay: new Date().getDay() === 0 ? 6 : new Date().getDay() - 1, // Indeks aktualnego dnia tygodnia
-            tab: new Date().getDay() === 0 ? 6 : new Date().getDay() - 1, // Ustawienie aktywnego dnia jako dzisiaj
+            days: ['Pon', 'Wt', 'Śr', 'Cz', 'Pt', 'So', 'Nd'],
+            currentDay: new Date().getDay() === 0 ? 6 : new Date().getDay() - 1,
+            selectedDay: null,
+            tab: new Date().getDay() === 0 ? 6 : new Date().getDay() - 1,
         };
+    },
+    methods: {
+        changeDay(index) {
+            this.tab = index;
+            this.selectedDay = index;
+        },
+    },
+    mounted() {
+        this.selectedDay = this.currentDay; // Przy kadym ładowaniu komponentu selectedDay z automatu jest dniem biezacym
     },
 };
 </script>
 
 <style scoped>
-@import "../styles/common-style.css";
+@import '../styles/common-style.css';
+
+
+/* te klasy to są klasy z komponentów z vuetify, potrzebowalam dostosowac je do naszego projektu */
+.v-tab.v-tab.v-btn {
+    min-width: 70px;
+    border-radius: 100px;
+    border: 1px solid #2f7d28;
+    margin-right: 1px;
+}
+
+.v-tab.v-tab.v-btn.active {
+    text-decoration: none !important;
+    background-color: rgba(47, 125, 40, 0.5);
+    color: white !important;
+}
 </style>
