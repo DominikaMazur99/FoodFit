9bad7ddd2c697d15d2ea667751da694fbbb70ecf
d.mazur <d.mazur@aotm.gov.pl>
2024-04-29 14:05:38 +0200

    details activity

--- src/components/journal/ActivityJournalComponent.vue
+++ src/components/journal/ActivityJournalComponent.vue
@@ -20,19 +20,44 @@
                 :key="index"
                 :value="index"
             >
+                <div class="training-box">
+                    <button
+                        @click="addTraining"
+                        class="add-btn"
+                    >
+                        Dodaj trening
+                     
+                    </button>
+                </div>
+                <activity-journal-details
+                    :data="data"
+                ></activity-journal-details>
             </v-window-item>
         </v-window>
     </div>
 </template>
 
 <script>
+import ActivityJournalDetails from './details/ActivityJournalDetails.vue';
+import SvgIcon from '@jamescoyle/vue-icon';
+import { mdiPlus } from '@mdi/js';
 export default {
+    components: {
+        'activity-journal-details': ActivityJournalDetails,
+        SvgIcon,
+    },
+
     data() {
         return {
             daysListToActivity: ['Pon', 'Wt', 'Śr', 'Cz', 'Pt', 'So', 'Nd'],
             currentDay: new Date().getDay() === 0 ? 6 : new Date().getDay() - 1,
             selectedDay: null,
             tab: new Date().getDay() === 0 ? 6 : new Date().getDay() - 1,
+            data: {
+                running: { name: 'bieganie', calories: 120 },
+                swimming: { name: 'pływanie', calories: 240 },
+            },
+            path: mdiPlus,
         };
     },
     methods: {
@@ -40,6 +65,9 @@ export default {
             this.tab = index;
             this.selectedDay = index;
         },
+        addTraining(){
+            console.log('add training')
+        }
     },
     mounted() {
         this.selectedDay = this.currentDay; // Przy kadym ładowaniu komponentu selectedDay z automatu jest dniem biezacym
@@ -49,7 +77,6 @@ export default {
 
 <style scoped>
 @import '../styles/common-style.css';
-
 
 /* te klasy to są klasy z komponentów z vuetify, potrzebowalam dostosowac je do naszego projektu */
 .v-tab.v-tab.v-btn {
@@ -62,6 +89,25 @@ export default {
 }
 
 .v-tab.v-tab.v-btn.active {
+    text-decoration: none !important;
+    background-color: rgba(47, 125, 40, 0.5);
+    color: white !important;
+}
+
+.training-box {
+    margin-top: 2rem;
+    display: flex;
+    align-items: left;
+    justify-content: space-around;
+}
+.add-btn{
+    border: 1px solid #2f7d28;;
+    padding: 0.5rem;
+    border-radius: 30px;
+    background-color: transparent;
+    cursor: pointer;
+}
+.add-btn:hover{
     text-decoration: none !important;
     background-color: rgba(47, 125, 40, 0.5);
     color: white !important;
