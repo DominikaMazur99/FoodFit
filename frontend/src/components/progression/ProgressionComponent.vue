98cb3408502aa1293a7eba6a0f0d7ec0d7c749e4
d.mazur <d.mazur@aotm.gov.pl>
2024-05-14 19:25:30 +0200

    chart generate well

--- frontend/src/components/progression/ProgressionComponent.vue
+++ frontend/src/components/progression/ProgressionComponent.vue
@@ -1,91 +1,205 @@
 <template>
-  <div class="common-box">
-    <Bar
-      id="my-chart-id"
-      :data="chartData"
-      :options="chartOptions"
-    />
-  </div>
+    <div class="common-box">
+        <div class="month-navigation">
+            <v-btn
+                icon
+                class="no-border"
+                @click="previousMonth"
+            >
+                <svg-icon
+                    type="mdi"
+                    :path="pathToBack"
+                    color="gray"
+                ></svg-icon>
+            </v-btn>
+            <span :style="{ 'font-size': '20px' }">{{ currentMonthName }}</span>
+            <v-btn
+                icon
+                class="no-border"
+                @click="nextMonth"
+            >
+                <svg-icon
+                    type="mdi"
+                    :path="pathToNext"
+                    color="gray"
+                ></svg-icon>
+            </v-btn>
+        </div>
+        <Bar
+            id="my-chart-id"
+            :data="chartData"
+            :options="chartOptions"
+        />
+    </div>
 </template>
 
 <script>
-import { Bar } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+import { Bar } from 'vue-chartjs';
+import {
+    Chart as ChartJS,
+    Title,
+    Tooltip,
+    Legend,
+    BarElement,
+    CategoryScale,
+    LinearScale,
+} from 'chart.js';
+import SvgIcon from '@jamescoyle/vue-icon';
+import { mdiChevronRight, mdiChevronLeft } from '@mdi/js';
 
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+ChartJS.register(
+    Title,
+    Tooltip,
+    Legend,
+    BarElement,
+    CategoryScale,
+    LinearScale
+);
 
 export default {
-  name: 'BarChart',
-  components: { Bar },
-  data() {
-    return {
-      activities: []
-    }
-  },
-  computed: {
-    chartData() {
-      const dailyCalories = Array(31).fill(0);
-
-      this.activities.forEach(activity => {
-        const activityDate = new Date(activity.date);
-        const year = activityDate.getFullYear();
-        const month = activityDate.getMonth();
-        const day = activityDate.getDate();
-
-        if (year === new Date().getFullYear() && month === 4) { // maj ma indeks 4
-          dailyCalories[day - 1] += activity.calories;
-        }
-      });
-
-      return {
-        labels: Array.from({ length: 31 }, (_, i) => i + 1),
-        datasets: [
-          {
-            label: 'Spalone kalorie w ciągu dnia',
-            data: dailyCalories,
-            backgroundColor: 'rgba(75, 192, 192, 0.2)',
-            borderColor: 'rgba(75, 192, 192, 1)',
-            borderWidth: 1
-          }
-        ]
-      };
+    name: 'BarChart',
+    components: {
+        Bar,
+        SvgIcon,
     },
-    chartOptions() {
-      return {
-        responsive: true,
-        scales: {
-          y: {
-            beginAtZero: true,
-            min: 0,
-            max: 1000
-          }
-        }
-      };
-    }
-  },
-  async mounted() {
-    const user = localStorage.getItem("login");
-    try {
-      const response = await fetch(
-        `http://localhost:3010/api/user-activities?userName=${user}`
-      );
-      const data = await response.json();
-      console.log("User activities:", data);
-      
-      if (!data.activities || !Array.isArray(data.activities)) {
-        console.error("Invalid activities data:", data.activities);
-        return;
-      }
+    data() {
+        return {
+            activities: [],
+            currentMonth: new Date().getMonth(), // starting from the current month
+            currentYear: new Date().getFullYear(), // current year
+            pathToNext: mdiChevronRight,
+            pathToBack: mdiChevronLeft,
+        };
+    },
+    computed: {
+        chartData() {
+            const daysInMonth = new Date(
+                this.currentYear,
+                this.currentMonth + 1,
+                0
+            ).getDate();
+            const dailyCalories = Array(daysInMonth).fill(0);
 
-      this.activities = data.activities;
-      console.log("Chart data:", this.chartData.datasets[0].data);
+            this.activities.forEach((activity) => {
+                const activityDate = new Date(activity.date);
+                const year = activityDate.getFullYear();
+                const month = activityDate.getMonth();
+                const day = activityDate.getDate();
 
-      // No need to force update as computed properties will reactively update the chart
-    } catch (error) {
-      console.error("Error fetching activities:", error);
-    }
-  }
-}
+                if (year === this.currentYear && month === this.currentMonth) {
+                    dailyCalories[day - 1] += activity.calories;
+                }
+            });
+
+            return {
+                labels: Array.from({ length: daysInMonth }, (_, i) => i + 1),
+                datasets: [
+                    {
+                        label: 'Spalone kalorie w ciągu dnia',
+                        data: dailyCalories,
+                        backgroundColor: '#2f7d28',
+                        borderColor: '#2f7d28',
+                        borderWidth: 1,
+                    },
+                ],
+            };
+        },
+
+        chartOptions() {
+            return {
+                responsive: true,
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                        min: 0,
+                        max: 1000,
+                        title: {
+                            display: true,
+                            text: 'Liczba spalonych kalorii', // opis osi y
+                            font: {
+                                weight: 'italic',
+                                size: '15px',
+                                color: 'gray'
+                            },
+                        },
+                    },
+                    x: {
+                      title: {
+                            display: true,
+                            text: 'Dzień', // opis osi x
+                            font: {
+                                weight: 'italic',
+                                size: '15px',
+                                color: 'gray'
+
+                            },
+                        },
+                    }
+                },
+            };
+        },
+        currentMonthName() {
+            const monthNames = [
+                'Styczeń',
+                'Luty',
+                'Marzec',
+                'Kwiecień',
+                'Maj',
+                'Czerwiec',
+                'Lipiec',
+                'Sierpień',
+                'Wrzesień',
+                'Październik',
+                'Listopad',
+                'Grudzień',
+            ];
+            return monthNames[this.currentMonth];
+        },
+    },
+    methods: {
+        async fetchActivities() {
+            const user = localStorage.getItem('login');
+            try {
+                const response = await fetch(
+                    `http://localhost:3010/api/user-activities?userName=${user}`
+                );
+                const data = await response.json();
+                console.log('User activities:', data);
+
+                if (!data.activities || !Array.isArray(data.activities)) {
+                    console.error('Invalid activities data:', data.activities);
+                    return;
+                }
+
+                this.activities = data.activities;
+                console.log('Chart data:', this.chartData.datasets[0].data);
+            } catch (error) {
+                console.error('Error fetching activities:', error);
+            }
+        },
+        previousMonth() {
+            if (this.currentMonth === 0) {
+                this.currentMonth = 11;
+                this.currentYear -= 1;
+            } else {
+                this.currentMonth -= 1;
+            }
+            this.fetchActivities();
+        },
+        nextMonth() {
+            if (this.currentMonth === 11) {
+                this.currentMonth = 0;
+                this.currentYear += 1;
+            } else {
+                this.currentMonth += 1;
+            }
+            this.fetchActivities();
+        },
+    },
+    mounted() {
+        this.fetchActivities();
+    },
+};
 </script>
 
 <style scoped>
@@ -106,5 +220,26 @@ export default {
     align-items: center; /* Wyśrodkowanie elementów w pionie */
     justify-content: center; /* Wyśrodkowanie elementów w poziomie */
     opacity: 0.85;
+}
+
+.month-navigation {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 8px;
+    font-size: 18px;
+    font-family: 'Jost';
+    margin-bottom: 5px;
+    color: gray;
+    font-style: italic;
+}
+
+.no-border {
+    border: none !important;
+    box-shadow: none !important;
+}
+
+.v-btn {
+    padding: 0;
 }
 </style>
